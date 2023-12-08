c5135dfd28b963522af8469fa27bfadad46414ff
Rachit Agarwal <rachitag112@gmail.com>
2023-12-08 20:58:55 +0530

    Changed dimensions for icons

--- components/Footer.js
+++ components/Footer.js
@@ -8,15 +8,15 @@ export default function Footer() {
                     <h3 className="text-3xl mb-3">Join Our Community Today!</h3>
                     <p>Explore the Realms of Web3 World</p>
                     <div className="flex justify-center my-10">
-                        <a href="https://twitter.com/daodeepverse" target="_blank" rel="noopener noreferrer" className="flex items-center border rounded-lg px-4 py-2 w-44 mx-2">
-                            <Image src="/twitterx.svg" alt="Twitter Icon" className="w-7 md:w-8"/>
+                        <a href="https://x.com/daodeepverse" target="_blank" rel="noopener noreferrer" className="flex items-center border rounded-lg px-4 py-2 w-44 mx-2">
+                            <Image src="/twitterx.svg" alt="Twitter Icon" width={500} height={500} className="w-7 md:w-8"/>
                             <div className="text-left ml-3">
                                 <p className='text-xs text-gray-200'>Follow us on</p>
                                 <p className="text-sm md:text-base">X</p>
                             </div>
                         </a>
-                        <a href="https://www.linkedin.com/daodeepverse/" target="_blank" rel="noopener noreferrer" className="flex items-center border rounded-lg px-4 py-2 w-44 mx-2">
-                            <Image src="/linkedin.svg" alt="LinkedIn Icon" className="w-7 md:w-8"/>
+                        <a href="https://www.linkedin.com/daodeepverse" target="_blank" rel="noopener noreferrer" className="flex items-center border rounded-lg px-4 py-2 w-44 mx-2">
+                            <Image src="/linkedin.svg" alt="LinkedIn Icon" width={500} height={500} className="w-7 md:w-8"/>
                             <div className="text-left ml-3">
                                 <p className='text-xs text-gray-200'>Follow us on</p>
                                 <p className="text-sm md:text-base">Linkedin</p>
